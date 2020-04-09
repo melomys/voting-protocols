dde08cb04baeaa9f5c0620fe81590fc7227e67ac
melomys <felix.matuschka@rwth-aachen.de>
2020-04-09 18:29:48 +0200

    do not use run

--- src/model.jl
+++ src/model.jl
@@ -8,6 +8,7 @@ using Distributions
 using LinearAlgebra
 using Random
 
+include("collect.jl")
 
 # Quality Dimensions
 quality_dimensions = 3
@@ -26,53 +27,25 @@ select_distribution = truncated(Normal(0, 2), 0, 1)
 
 
 mutable struct User <: AbstractAgent
-    id::Int
+    id::Int64
     quality_perception::Array
     vote_probability::Float64
 end
 
-
-function create_params(;
-    start_posts = 10,
-    start_users = 100,
-    new_user_probability = 0,
-    new_content_probability = 0.1,
-    vote_probability_scale = 10,
-    frontpagesize = 10,
-    seed = 19,
-)
-
-    votes = zeros(Int, start_posts)
-    quality = rand(quality_distribution, start_posts)
-    timestamps = zeros(Int, start_posts)
-    scores = zeros(Int, start_posts)
-
-    params = @dict(
-        votes,
-        quality,
-        timestamps,
-        scores,
-        start_users,
-        new_user_probability,
-        new_content_probability,
-        vote_probability_scale,
-        frontpagesize,
-        seed
-    )
-    println(start_users)
-    println(params[:start_users])
-    return params
+mutable struct Post <: AbstractAgent
+    id::Int64
+    quality::Vector{Float64}
+    votes::Int64
+    timestamp::Int64
+    score::Float64
 end
 
 function scoring(votes, timestamp, time)
-    -votes * (time - timestamp)
+    - (votes+1) * (time - timestamp)
 end
 
 function model_initiation(;
-    votes,
-    quality,
-    timestamps,
-    scores,
+    start_posts = 10,
     start_users = 100,
     new_user_probability = 0,
     new_content_probability = 0.1,
@@ -81,6 +54,12 @@ function model_initiation(;
     seed = 0
 )
     Random.seed!(seed)
+
+    votes = zeros(Int, start_posts)
+    quality = rand(quality_distribution, start_posts)
+    timestamps = zeros(Int, start_posts)
+    scores = zeros(Int, start_posts)
+
     n = length(votes)
     window = [1:n...]
 
@@ -122,7 +101,6 @@ function agent_step!(agent, model)
 end
 
 function model_step!(model)
-    println("MODEL_STEP")
     if rand() < model.properties[:new_content_probability]
         append!(model.properties[:quality], rand(quality_distribution))
         append!(model.properties[:votes], 0)
@@ -131,8 +109,7 @@ function model_step!(model)
         model.properties[:n] += 1
     end
     if rand() < model.properties[:new_user_probability]
-        add_agent!(
-            step!model,
+        add_agent!(model,
             rand(quality_distribution),
             rand() / model.properties[:vote_probability],
         )
@@ -151,17 +128,37 @@ function model_step!(model)
         1:model.properties[:frontpagesize],
     )
     model.properties[:window] = votes_idx
-
 end
 
-params = create_params()
-model = model_initiation(;params...)
 
-#model_properties = Dict(:votes => [mean, median])
-agent_properties = Dict(:vote_probability => [sum])
+function run!(model, agent_step!,model_step!,n; when = true, when_model = when, agent_properties=nothing, model_properties=nothing)
+        df_agent = init_agent_dataframe(model, agent_properties)
+        df_model = init_model_dataframe(model, model_properties)
+        if n isa Integer
+            if when == true; for c in eachcol(df_agent); sizehint!(c, n); end; end
+            if when_model == true; for c in eachcol(df_model); sizehint!(c, n); end; end
+        end
+
+        for s in 0:n
+            collect_agent_data!(df_agent, model, agent_properties, s)
+            collect_model_data!(df_model, model, model_properties, s)
+            step!(model, agent_step!, model_step!, 1)
+        end
+        return df_agent, df_model
+end
 
 
 
-data = step!(model, agent_step!,3, agent_properties)
+
+
+model = model_initiation()
+
+model_properties = [:votes, :scores]
+agent_properties = [:vote_probability]
+
+model_df = init_model_dataframe(model, model_properties)
+agent_df = init_agent_dataframe(model, agent_properties)
+
+agent_df, model_df = run!(model, agent_step!, model_step!, 30, agent_properties=agent_properties, model_properties = model_properties)
+data = step!(model, agent_step!,model_step!,3, agent_properties)
 println(data)
-println("running thru")
