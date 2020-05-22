53b0655504ae1b6af915efa50a89900e3c1f6463
melomys <felix.matuschka@rwth-aachen.de>
2020-05-22 11:33:19 +0200

    del plot_helper.jl

--- src/plot_helper.jl
+++
@@ -1,4 +0,0 @@
-function unpack_data(data;padding=0)
-    ncols = maximum(map(length, data))
-    DataFrame(Matrix(DataFrame([vcat(data[i], ones(ncols - length(data[i]))*padding) for i in 1:length(data)]))')
-end
