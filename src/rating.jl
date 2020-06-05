1782fd88219b4cda5aa2224fcdfbb8926df4d001
melomys <felix.matuschka@rwth-aachen.de>
2020-06-05 12:46:46 +0200

    adding distance rating function

--- src/rating.jl
+++ src/rating.jl
@@ -5,3 +5,11 @@ end
 function user_rating_exp(post_quality, user_quality_perception)
     reduce(*,sigmoid.(post_quality).^(sigmoid.(user_quality_perception)))
 end
+
+function user_rating_exp2(post_quality, user_quality_perception)
+    sum(sigmoid.(post_quality).^(sigmoid.(user_quality_perception)))
+end
+
+function user_rating_dist1(post_quality, user_quality_perception)
+    1 - sum(abs.(sigmoid.(post_quality) - sigmoid.(user_quality_perception)))/length(post_quality)
+end
