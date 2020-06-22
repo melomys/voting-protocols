b2433d53313826c6e72624315be83c779e571642
melomys <felix.matuschka@rwth-aachen.de>
2020-06-22 14:13:38 +0200

    add function: quality_first_quantile

--- src/evaluation.jl
+++ src/evaluation.jl
@@ -1,3 +1,6 @@
+# Per iteration
+# parameters: model
+
 function ranking_rating(model)
     rating = 0
     for i = 1:model.n
@@ -19,10 +22,9 @@ function ranking_rating_relative(model)
     ranking_rating(model)/rating
 end
 
-function trapezoidial_rule(points)
-    sum(points) - 1 / 2 * (points[1] + points[end])
-end
 
+# per model
+# parameters: model, model_df
 
 function area_under_curve(model, model_df)
     trapezoidial_rule(model_df[!, :ranking_rating])
@@ -34,7 +36,7 @@ function sum_gradient(model, model_df)
 end
 
 quality_sum(model, model_df) = sum(map(
-    x -> model.user_rating_function(x.quality, ones(quality_dimensions)),
+    x -> model.user_rating_function(x.quality, ones(model.quality_dimensions)),
     model.posts,
 ))
 
@@ -43,16 +45,29 @@ function gain(model_df)
     sign(model_df[end, :ranking_rating] - model_df[2, :ranking_rating])
 end
 
-function mean_user_view(model)
+function mean_user_view(model, model_df)
     mean(x -> length(x.viewed)/model.n, allagents(model))
 end
 
-function mean_user_vote(model)
+function mean_user_vote(model, model_df)
     mean(x -> length(x.voted_on)/model.n,allagents(model))
 end
 
+function quality_first_quantile(model, model_df)
+    max_score = model.posts[model.ranking[1]].score
+    min_score = model.posts[model.ranking[end]].score
+    quality_quantile = 0
+    for post in model.posts
+        if  post.score >= 0.75*(max_score-min_score)
+            quality_quantile += model.user_rating_function(post.quality, ones(model.quality_dimensions))
+        end
+    end
+    return quality_quantile/quality_sum(model,model_df)
+end
 
-# Evaluation inner rating
+
+# per post in models
+# parameters: post, model, model_df
 
 function end_position(post, model, model_df)
     index = findfirst(isequal(post), model.posts)
@@ -63,7 +78,12 @@ function quality(post,model,model_df)
     model.user_rating_function(post.quality, ones(model.quality_dimensions))
 end
 
+# Helpers
 
+
+function trapezoidial_rule(points)
+    sum(points) - 1 / 2 * (points[1] + points[end])
+end
 
 
 """
