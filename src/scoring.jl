c9028a545823abba1aece6e21806e0bb66baab56
melomys <felix.matuschka@rwth-aachen.de>
2020-06-30 13:34:07 +0200

    add reddit best comment scoring

--- src/scoring.jl
+++ src/scoring.jl
@@ -15,11 +15,23 @@ end
 epoch = DateTime(1970,1,1)
 start_time = DateTime(2020,06,20)
 
-function scoring_reddit(post, time, model)
+function scoring_reddit_hot(post, time, model)
     seconds = Dates.value(start_time - epoch)/1000 - 1134028003 + post.timestamp * 60 * 30 # 60 Sekunden pro 30 Minuten!!
-    order = log(10, max(abs(post.votes),1))
+    order = log(10, max(abs(post.votes-post.downvotes),1))
     round(sign(post.votes)*order + seconds/45000; digits=7)
 end
+
+function scoring_reddit_best(post,time,model)
+    confidence = 0.95
+    if n == 0
+        return 0
+    end
+    z = Statistics.quantile(Normal(), 1-(1-confidence)/2)
+    n = post.votes + post.downvotes
+    phat = 1.0*post.votes/n
+    (phat + z*z/(2*n) - z * sqrt(phat*(1- phat) + z*z/(4*n)/n)) / (1 + (z*z)/n)
+end
+
 
 function scoring_random(post, time, model)
     rand()
@@ -35,13 +47,13 @@ end
 
 
 function scoring_activation(post, time,model)
-    (post.votes - post.score) / (time -post.timestamp + 1)^(0.3)
+    (post.votes - post.score) / (time -post.timestamp + 1)^(model.time_exp)
 end
 
 function scoring_votes_divided_score(post, time, model)
-    (post.votes)/(post.score+1) / (time - post.timestamp + 1)^(0.5)
+    (post.votes)/(post.score+1) / (time - post.timestamp + 1)^(model.time_exp)
 end
 
 function scoring_votes_times_score(post, time, model)
-    post.votes * (post.score + 1) / (time- post.timestamp + 1)^(0.5)
+    post.votes * (post.score + 1) / (time- post.timestamp + 1)^(model.time_exp)
 end
