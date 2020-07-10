c0f8b127054dbff6c35fd3c6b07f5bd312d21910
melomys <felix.matuschka@rwth-aachen.de>
2020-07-10 22:16:36 +0200

    fix numbering of export files

--- src/export_r.jl
+++ src/export_r.jl
@@ -5,7 +5,12 @@ function export_rds(df, model_dfs, keyword = "")
     files = cd(readdir,"data")
     numbers = map(x -> parse(Int32,match(r"([0-9]+)",x)[1]),files)
 
-    no = maximum(numbers) + 1
+    if length(numbers == 0)
+        no = 1
+    else
+        no = maximum(numbers) + 1
+    end
+
 
     if keyword == ""
         df_file = "data/df$(no).rds"
