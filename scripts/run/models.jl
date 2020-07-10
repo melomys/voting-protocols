59d3a12007b805b4d83aca3e544e22e181692610
melomys <felix.matuschka@rwth-aachen.de>
2020-07-10 19:46:43 +0200

    experiments and run/models.jl

--- scripts/run/models.jl
+++ scripts/run/models.jl
@@ -1,8 +1,3 @@
-"""
-plots the correlation matrix between given features of the model
-"""
-
-
 using Agents
 using DataFrames
 using StatsPlots
@@ -25,10 +20,6 @@ include("../../src/default.jl")
 timestamp_func = @post_property_function(:timestamp)
 score_func = @post_property_function(:score)
 
-
-step = 100
-
-
 evaluation_functions = default_evaluation_functions
 
 model_properties = [
@@ -42,7 +33,7 @@ model_properties = [
     @get_post_data(:quality, identity),
 ]
 
-model_init_params2 = [
+model_init_params = [
     (
         :all_models,
         Dict(
@@ -68,7 +59,7 @@ model_init_params2 = [
         [standard_model, random_model],
         Dict(
             :scoring_function => [scoring_activation, scoring_hacker_news],
-            :init_score => [0:10:100],
+            :init_score => [0:10:100...],
             :deviation_function => [mean_deviation, std_deviation],
         ),
     ),
@@ -91,46 +82,13 @@ model_init_params2 = [
     ),
 ]
 
-
-model_init_params_user = [(
-    standard_model,
-    Dict(
-    :scoring_function => scoring_activation,
-    :init_score => 10,
-    :user_rating_function => [user_rating_exp, user_rating_exp2, user_rating_dist2],
-    :concentration_distribution => [Uniform(10,70), Uniform(10, 100), Uniform(30,70), Uniform(30,100), Uniform(70,100)],
-    :activity_voting_probability_distribution => [MvLogNormal(MvNormal([-2, 0], [1.0 0.8; 0.8 1.0])),
-    MvLogNormal(MvNormal([-2, 0], [1.0 0.8; 0.8 1.0])),MvLogNormal(MvNormal([-2, 0], [1.0 0.8; 0.8 1.0])),
-    MvLogNormal(MvNormal([0, 0], [1.0 0.8; 0.8 1.0])), MvLogNormal(MvNormal([-2, -2], [1.0 0.8; 0.8 1.0])),
-    ],
-    :quality_dimensions => [1:3...]
-        )
-
-)]
-
-model_init_params_rating_function = [(
-    downvote_model,
-    Dict(
-    :scoring_function => scoring_reddit_hot,
-    :user_rating_function => [user_rating, user_rating_exp, user_rating_exp2, user_rating_dist2]
-    )
-)]
-
-model_init_params_concentration = [(
-    standard_model,
-    Dict(
-    :scoring_function => scoring_reddit_hot,
-    :concentration_distribution => [Uniform(10,70), Uniform(10, 100), Uniform(30,70), Uniform(30,100), Uniform(70,100)],
-    )
-)]
-
 @time begin
     model_dfs, corr_df = collect_model_data(
-        model_init_params_concentration,
+        model_init_params,
         default_model_properties,
         default_evaluation_functions,
         1000,
     )
 end
 
-export_rds(corr_df, model_dfs, "concentration")
+export_rds(corr_df, model_dfs, "models")
