1fd12201f2a43d4b8eeb0f206bbcd2262cbb5102
melomys <felix.matuschka@rwth-aachen.de>
2020-07-25 12:37:30 +0200

    fixing /run/voting_probability.jl

--- scripts/run/activtiy_voting_probabilty.jl
+++ scripts/run/activtiy_voting_probabilty.jl
@@ -1,14 +1,15 @@
 using Distributed
 println(nprocs())
 @time @everywhere using VotingProtocols
+using Distributions
 
 
 model_init_params = [default_models...,
     (
         :all_models,
         Dict(
-        :activity_distribution => [Beta(2,5,5), Beta(1,5), Beta(1, 10), Beta(2.5,10, Beta(5,10), Beta(7.5,10))],
-        :voting_probability_distribution => [Beta(2,5,5), Beta(1,5), Beta(1, 10), Beta(2.5,10, Beta(5,10), Beta(7.5,10))]
+        :activity_distribution => [Beta(2.5,5), Beta(1,5), Beta(1, 10), Beta(2.5,10), Beta(5,10), Beta(7.5,10)],
+        :voting_probability_distribution => [Beta(2.5,5), Beta(1,5), Beta(1, 10), Beta(2.5,10), Beta(5,10), Beta(7.5,10)]
         ),
     ),
 ]
