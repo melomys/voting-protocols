1f9bf5d8cdd4bc159401fb5abc73a672590b0383
melomys <felix.matuschka@rwth-aachen.de>
2020-07-31 00:04:28 +0200

    refresh default models

--- src/default.jl
+++ src/default.jl
@@ -58,7 +58,7 @@ default_evaluation_functions = [
 
 sort!(default_evaluation_functions, by = x -> string(x))
 
-
+"""
 default_models = [
 (
     downvote_model,
@@ -78,3 +78,160 @@ default_models = [
         :relevance_gravity => [0,1.8],
         :deviation_function => [no_deviation, mean_deviation]
 ))]
+"""
+default_models = [
+(
+    downvote_model,
+    Dict(
+        :scoring_function => scoring_hacker_news,
+        :init_score => 50,
+        :vote_evaluation => vote_difference,
+        :deviation_function => no_deviation,
+        :gravity => 0,
+        :relevance_gravity => 0,
+        :user_rating_function => user_rating_exp2,
+    ),
+    ),(downvote_model,
+    Dict(
+        :scoring_function => scoring_hacker_news,
+        :init_score => 50,
+        :vote_evaluation => vote_difference,
+        :deviation_function => mean_deviation,
+        :gravity => 2,
+        :relevance_gravity => 2,
+        :user_rating_function => user_rating_exp2,
+    )),(downvote_model,
+    Dict(
+        :scoring_function => scoring_hacker_news,
+        :init_score => 50,
+        :vote_evaluation => vote_difference,
+        :deviation_function => mean_deviation,
+        :gravity => 2,
+        :relevance_gravity => 0,
+        :user_rating_function => user_rating_dist2,
+    )),(downvote_model,
+    Dict(
+        :scoring_function => scoring_hacker_news,
+        :init_score => 50,
+        :vote_evaluation => vote_difference,
+        :deviation_function => mean_deviation,
+        :gravity => 2,
+        :relevance_gravity => 2,
+        :user_rating_function => user_rating_dist2,
+    )),
+    (
+        downvote_model,
+        Dict(
+            :scoring_function => scoring_view,
+            :init_score => 20,
+            :vote_evaluation => vote_difference,
+            :deviation_function => mean_deviation,
+            :gravity => 0,
+            :relevance_gravity => 0,
+            :user_rating_function => user_rating_exp2,
+        ),
+        ),(downvote_model,
+        Dict(
+            :scoring_function => scoring_view,
+            :init_score => 30,
+            :vote_evaluation => wilson_score,
+            :deviation_function => mean_deviation,
+            :gravity => 2,
+            :relevance_gravity => 2,
+            :user_rating_function => user_rating_exp2,
+        )),(downvote_model,
+        Dict(
+            :scoring_function => scoring_view,
+            :init_score => 30,
+            :vote_evaluation => wilson_score,
+            :deviation_function => mean_deviation,
+            :gravity => 2,
+            :relevance_gravity => 0,
+            :user_rating_function => user_rating_dist2,
+        )),(downvote_model,
+        Dict(
+            :scoring_function => scoring_view,
+            :init_score => 30,
+            :vote_evaluation => wilson_score,
+            :deviation_function => mean_deviation,
+            :gravity => 2,
+            :relevance_gravity => 2,
+            :user_rating_function => user_rating_dist2,
+        )),
+        (
+            downvote_model,
+            Dict(
+                :scoring_function => scoring_activation,
+                :init_score => 10,
+                :vote_evaluation => vote_difference,
+                :deviation_function => no_deviation,
+                :gravity => 2,
+                :relevance_gravity => 0,
+                :user_rating_function => user_rating_exp2,
+            ),
+            ),(downvote_model,
+            Dict(
+                :scoring_function => scoring_activation,
+                :init_score => 30,
+                :vote_evaluation => vote_difference,
+                :deviation_function => mean_deviation,
+                :gravity => 2,
+                :relevance_gravity => 2,
+                :user_rating_function => user_rating_exp2,
+            )),(downvote_model,
+            Dict(
+                :scoring_function => scoring_activation,
+                :init_score => 30,
+                :vote_evaluation => vote_difference,
+                :deviation_function => mean_deviation,
+                :gravity => 2,
+                :relevance_gravity => 0,
+                :user_rating_function => user_rating_dist2,
+            )),(downvote_model,
+            Dict(
+                :scoring_function => scoring_activation,
+                :init_score => 30,
+                :vote_evaluation => vote_difference,
+                :deviation_function => mean_deviation,
+                :gravity => 2,
+                :relevance_gravity => 2,
+                :user_rating_function => user_rating_dist2,
+            )),
+            (
+                downvote_model,
+                Dict(
+                    :scoring_function => scoring_reddit_hot,
+                    :init_score => 30000,
+                    :vote_evaluation => vote_difference,
+                    :deviation_function => no_deviation,
+                    :relevance_gravity => 0,
+                    :user_rating_function => user_rating_exp2,
+                ),
+                ),(downvote_model,
+                Dict(
+                    :scoring_function => scoring_reddit_hot,
+                    :init_score => 30000,
+                    :vote_evaluation => vote_difference,
+                    :deviation_function => mean_deviation,
+                    :relevance_gravity => 2,
+                    :user_rating_function => user_rating_exp2,
+                )),(downvote_model,
+                Dict(
+                    :scoring_function => scoring_reddit_hot,
+                    :init_score => 30000,
+                    :vote_evaluation => vote_difference,
+                    :deviation_function => mean_deviation,
+                    :relevance_gravity => 0,
+                    :user_rating_function => user_rating_dist2,
+                )),(downvote_model,
+                Dict(
+                    :scoring_function => scoring_reddit_hot,
+                    :init_score => 30000,
+                    :vote_evaluation => vote_difference,
+                    :deviation_function => mean_deviation,
+                    :relevance_gravity => 2,
+                    :user_rating_function => user_rating_dist2,
+                )),
+
+)
+]
