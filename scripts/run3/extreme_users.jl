0f3a1d856cedb7959e2b3f5271c502925aa40227
melomys <felix.matuschka@rwth-aachen.de>
2020-07-31 12:05:35 +0200

    add more extreme users

--- scripts/run3/extreme_users.jl
+++ scripts/run3/extreme_users.jl
@@ -7,7 +7,7 @@ model_init_params = [default_models...,
     (
         :all_models,
         Dict(
-            :user => [[(0.05, extreme_user(1)),(0.95, user())],[(0.1, extreme_user(1)),(0.9, user())],[(0.2, extreme_user(1)),(0.8, user())]]
+            :user => [user(),[(0.05, extreme_user(1)),(0.95, user())],[(0.1, extreme_user(1)),(0.9, user())],[(0.2, extreme_user(1)),(0.8, user())],[(0.1, extreme_user(1)),(0.8, user()),(0.1, extreme_user(-1))]]
         ),
     ),
 ]
