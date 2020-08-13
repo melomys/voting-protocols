1d80c063558e8e043b3b5f163be422140c4a3dc3
melomys <felix.matuschka@rwth-aachen.de>
2020-08-13 22:26:31 +0200

    experiments

--- scripts/experiments/metrics.jl
+++ scripts/experiments/metrics.jl
@@ -90,6 +90,21 @@ model_init_params = [(
 ]
 
 
+d1 = MvNormal(ones(3), I(3))
+d2 = MvNormal([-4,0,5],[1 0.5 0.9; 0.5 1 0.1;0.9 0.1 1])
+d3 = MvNormal([-4,0,5],[1 0.1 0; 0.1 1 0; 0 0 1])
+d4 = MvNormal([0,0,0],[1 0.5 0.9; 0.5 1 0.1;0.9 0.1 1])
+
+model_init_params = [default_models...,
+    (
+        :all_models,
+        Dict(
+            :quality_distribution => [d1,d2,d3,d4],
+        ),
+    ),
+]
+
+
 seed_ = abs(rand(Int))
 
 seed_ = 3
